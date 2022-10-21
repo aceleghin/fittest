f18f0e39ba5db13489e65572c5523177aa80a521
aceleghin <alessandro@optix.cloud>
2022-10-21 21:26:19 +0200

    added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// Import the functions you need from the SDKs you need
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { AngularFireModule } from '@angular/fire/compat';
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import {AngularFireAnalyticsModule} from "@angular/fire/compat/analytics";
+import {AngularFirestoreModule} from "@angular/fire/compat/firestore";
+
 
 @NgModule({
   declarations: [
@@ -10,9 +21,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAnalyticsModule,
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+
+  // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebaseConfig: {
+    apiKey: "AIzaSyDfnYNiseb6AdARi8Xntvr7CRYzJb3xcbc",
+    authDomain: "fittest-42667.firebaseapp.com",
+    projectId: "fittest-42667",
+    storageBucket: "fittest-42667.appspot.com",
+    messagingSenderId: "915802596846",
+    appId: "1:915802596846:web:7782647f2774b2e6cd1aff",
+    measurementId: "G-7LW3KWCFX0"}
+  };
 
 /*
  * For easier debugging in development mode, you can import the following file
