ce219f2374111195b989f3ae0d3cdc534a1cee81
aceleghin <alessandro@optix.cloud>
2022-10-21 21:33:01 +0200

    added firebase config for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDfnYNiseb6AdARi8Xntvr7CRYzJb3xcbc",
+    authDomain: "fittest-42667.firebaseapp.com",
+    projectId: "fittest-42667",
+    storageBucket: "fittest-42667.appspot.com",
+    messagingSenderId: "915802596846",
+    appId: "1:915802596846:web:7782647f2774b2e6cd1aff",
+    measurementId: "G-7LW3KWCFX0"
+  }
+}
